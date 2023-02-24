eccbc7d54c5ac33630d3e4f27bb821be022ce7e0
BadaLee2000 <badal@uw.edu>
2023-02-23 21:08:00 -0800

    bada

--- Chart3.R
+++ Chart3.R
@@ -2,13 +2,12 @@ seattle_df <- read.csv("~/Desktop/2022-2023-All-Checkouts-SPL-Data.csv", strings
 x_values <- seq(1, 3)
 y_values <- seq(1,3)
 
-
-trend2 <- seattle_df %>%  filter(MaterialType == "EBOOK") %>% filter(CheckoutYear != 2023) 
-
+trend1 <- seattle_df %>%  filter(MaterialType == "BOOK") %>% 
+  filter(CheckoutMonth != 1) 
 
 library(ggplot2)
-ggplot(trend2) +
+ggplot(trend1) +
   geom_col(aes(x=CheckoutMonth, y = Checkouts, fill = Checkouts)) +
-  labs(title = "Number of Checkouts for EBooks in 2022",
+  labs(title = "Number of Checkouts for Printed Books in 2022",
        x = "Months",
        y = "Checkouts") 
--- chart2_example.R
+++ chart2_example.R
@@ -1,6 +1,13 @@
+seattle_df <- read.csv("~/Desktop/2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = TRUE)
 x_values <- seq(1, 3)
 y_values <- seq(1,3)
 
+trend1 <- seattle_df %>%  filter(MaterialType == "BOOK") %>% 
+  filter(CheckoutMonth != 1) 
+
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+ggplot(trend1) +
+  geom_col(aes(x=CheckoutMonth, y = Checkouts, fill = Checkouts)) +
+  labs(title = "Number of Checkouts for Printed Books in 2022",
+       x = "Months",
+       y = "Checkouts") 
